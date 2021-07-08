60cb1835cf307a6ce8c0b457caeab5caebf3a716
clyntonjae <clynton.manaois@fumaco.com>
2021-07-08 12:51:02 +0800

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -305,9 +305,11 @@ class MainController extends Controller
 
         $total_issued += DB::table('tabAthena Transactions as at')
             ->join('tabPacking Slip as ps', 'ps.name', 'at.reference_parent')
+            ->join('tabPacking Slip Item as psi', 'ps.name', 'ps.parent')
             ->join('tabDelivery Note as dr', 'ps.delivery_note', 'dr.name')
             ->whereIn('at.reference_type', ['Packing Slip', 'Picking Slip'])
             ->where('dr.docstatus', 0)->where('ps.docstatus', '<', 2)
+            ->where('psi.status', 'Issued')
             ->where('at.item_code', $item_code)->where('at.source_warehouse', $warehouse)
             ->sum('at.issued_qty');
 
@@ -1930,42 +1932,25 @@ class MainController extends Controller
         try {
             $now = Carbon::now();
 
-            $stock_entry_detail = DB::table('tabStock Entry Detail')->where('parent', $stock_entry)->get();
+            $stock_entry_detail = DB::connection('mysql')->table('tabStock Entry Detail')->where('parent', $stock_entry)->get();
 
-            $latest_id = DB::table('tabBin')->max('name');
+            $latest_id = DB::connection('mysql')->table('tabBin')->max('name');
             $latest_id_exploded = explode("/", $latest_id);
             $new_id = $latest_id_exploded[1] + 1;
 
+            $stock_entry_qry = DB::connection('mysql')->table('tabStock Entry')->where('name', $stock_entry)->first();
+
+            $stock_entry_detail = DB::connection('mysql')->table('tabStock Entry Detail')->where('parent', $stock_entry)->get();
+            
+            $s_data_insert = [];
+            $d_data = [];
             foreach($stock_entry_detail as $row){
-                if($row->s_warehouse){
-                    $bin_qry = DB::table('tabBin')->where('warehouse', $row->s_warehouse)
+               
+                    if($row->s_warehouse){
+                        $bin_qry = DB::connection('mysql')->table('tabBin')->where('warehouse', $row->s_warehouse)
                         ->where('item_code', $row->item_code)->first();
-
-                    if(!$bin_qry) {
-                        return ['success' => false, 'message' => 'Insufficient stock for <b>' . $row->item_code . '</b> in <b>' . $row->s_warehouse . '</b>.'];
-                    }
-
-                    $actual_qty_after_transaction = ($bin_qry->actual_qty - $row->transfer_qty);
-                    if($actual_qty_after_transaction <= 0) {
-                        return ['success' => false, 'message' => 'Insufficient stock for <b>' . $row->item_code . '</b> in <b>' . $row->s_warehouse . '</b>.'];
-                    }
-
-                    $bin = [
-                        'modified' => $now->toDateTimeString(),
-                        'modified_by' => Auth::user()->wh_user,
-                        'actual_qty' => $actual_qty_after_transaction,
-                        'stock_value' => $bin_qry->valuation_rate * $row->transfer_qty,
-                        'valuation_rate' => $bin_qry->valuation_rate,
-                    ];
-    
-                    DB::table('tabBin')->where('name', $bin_qry->name)->update($bin);
-                }
-
-                if($row->t_warehouse){
-                    $bin_qry = DB::table('tabBin')->where('warehouse', $row->t_warehouse)
-                        ->where('item_code', $row->item_code)->first();
-
                     if (!$bin_qry) {
+                               
                         $new_id = $new_id + 1;
                         $new_id = str_pad($new_id, 7, '0', STR_PAD_LEFT);
                         $id = 'BINM/'.$new_id;
@@ -1974,8 +1959,64 @@ class MainController extends Controller
                             'name' => $id,
                             'creation' => $now->toDateTimeString(),
                             'modified' => $now->toDateTimeString(),
-                            'modified_by' => Auth::user()->wh_user,
-                            'owner' => Auth::user()->wh_user,
+                            'modified_by' => Auth::user()->email,
+                            'owner' => Auth::user()->email,
+                            'docstatus' => 0,
+                            'parent' => null,
+                            'parentfield' => null,
+                            'parenttype' => null,
+                            'idx' => 0,
+                            'reserved_qty_for_production' => 0,
+                            '_liked_by' => null,
+                            'fcfs_rate' => 0,
+                            'reserved_qty' => 0,
+                            '_assign' => null,
+                            'planned_qty' => 0,
+                            'item_code' => $row->item_code,
+                            'actual_qty' => $row->transfer_qty,
+                            'projected_qty' => $row->transfer_qty,
+                            'ma_rate' => 0,
+                            'stock_uom' => $row->stock_uom,
+                            '_comments' => null,
+                            'ordered_qty' => 0,
+                            'reserved_qty_for_sub_contract' => 0,
+                            'indented_qty' => 0,
+                            'warehouse' => $row->s_warehouse,
+                            'stock_value' => $row->valuation_rate * $row->transfer_qty,
+                            '_user_tags' => null,
+                            'valuation_rate' => $row->valuation_rate,
+                        ];
+
+                        DB::connection('mysql')->table('tabBin')->insert($bin);
+                    }else{
+                        $bin = [
+                            'modified' => $now->toDateTimeString(),
+                            'modified_by' => Auth::user()->email,
+                            'actual_qty' => $bin_qry->actual_qty - $row->transfer_qty,
+                            'stock_value' => $bin_qry->valuation_rate * $row->transfer_qty,
+                            'valuation_rate' => $bin_qry->valuation_rate,
+                        ];
+        
+                        DB::connection('mysql')->table('tabBin')->where('name', $bin_qry->name)->update($bin);
+                    }
+                    
+                }
+
+                if($row->t_warehouse){
+                    $bin_qry = DB::connection('mysql')->table('tabBin')->where('warehouse', $row->t_warehouse)
+                        ->where('item_code', $row->item_code)->first();
+                    if (!$bin_qry) {
+                        
+                        $new_id = $new_id + 1;
+                        $new_id = str_pad($new_id, 7, '0', STR_PAD_LEFT);
+                        $id = 'BINM/'.$new_id;
+
+                        $bin = [
+                            'name' => $id,
+                            'creation' => $now->toDateTimeString(),
+                            'modified' => $now->toDateTimeString(),
+                            'modified_by' => Auth::user()->email,
+                            'owner' => Auth::user()->email,
                             'docstatus' => 0,
                             'parent' => null,
                             'parentfield' => null,
@@ -2002,24 +2043,23 @@ class MainController extends Controller
                             'valuation_rate' => $row->valuation_rate,
                         ];
 
-                        DB::table('tabBin')->insert($bin);
+                        DB::connection('mysql')->table('tabBin')->insert($bin);
                     }else{
                         $bin = [
                             'modified' => $now->toDateTimeString(),
-                            'modified_by' => Auth::user()->wh_user,
+                            'modified_by' => Auth::user()->email,
                             'actual_qty' => $bin_qry->actual_qty + $row->transfer_qty,
                             'stock_value' => $bin_qry->valuation_rate * $row->transfer_qty,
                             'valuation_rate' => $bin_qry->valuation_rate,
                         ];
         
-                        DB::table('tabBin')->where('name', $bin_qry->name)->update($bin);
+                        DB::connection('mysql')->table('tabBin')->where('name', $bin_qry->name)->update($bin);
                     }
                 }
             }
             
-            return ['success' => true, 'message' => 'Bin updated.'];
         } catch (Exception $e) {
-            return ['success' => false, 'message' => $e->getMessage()];
+            return response()->json(["error" => $e->getMessage(), 'id' => $stock_entry]);
         }
     }
 	
