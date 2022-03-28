5b30d738ce668fb7632c0a9f3c15c13c773fc8e6
javekulong <85483372+javekulong@users.noreply.github.com>
2022-03-28 15:48:14 +0800

    AI-182
    Single Item Display of Details per row

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -24,109 +24,178 @@
 							</a>
  						@endforeach
 					</div>
-					<div class="card card-gray card-outline">
-						<div class="card-header p-0">
-							<div class="row">
-								<div class="col-md-6">
-									<h5 class="card-title mt-2 ml-4 font-weight-bold" style="font-size: 14px;">
-										@if(request('searchString') && request('searchString') != '') 
-										Search result(s) for "{{ request('searchString') }}"
-									@else
-										Item List
-									@endif</h5>
+					<div class="row border border-secondary">
+						<div class="card card-gray d-none d-xl-block card-outline col-2">
+							{{-- <div class="card mb-3">
+								<div class="card-header text-white font-weight-bold" style="font-size: 0.75rem; background-color: #001F3F;">Filters</div>
+								<div class="card-body">
+									@foreach ($itemClass as $itemClass1)
+										<div class="form-check">
+											<input type="checkbox" class="form-check-input" name="{{ $itemClass1->item_classification }}" value="{{ $itemClass1->item_classification}}">
+											<label class="form-check-label" style="font-size: 0.8rem;">{{ $itemClass1->item_classification }}</label>
+										</div>
+									@endforeach
 								</div>
-								<div class="col-md-6">
-									<div class="row">
-										<div class="col-md-6 p-1">
-											<div class="form-group m-0 w-55" id="warehouse-filter-parent" style="font-size: 11pt;">
-												<select name="warehouse" id="warehouse-filter" class="form-control">
-													
-												</select>
-											</div>
-										</div>
-										<div class="col-7 col-md-3 p-1 text-center">
-											<div class="form-group m-0r">
-												<label>
-													<input type="checkbox" class="minimal" id="cb-2" {{ (request('check_qty')) ? 'checked' : null }} >
-													
-													<span style="font-size: 12px;">Remove zero-qty items</span>
-												</label>
-											</div>
-										</div>
+							</div> --}}
+						</div>
+						<div class="card card-gray card-outline col-12 col-xl-10">
+							<div class="card-header p-0">
+								<div class="row">
+									{{-- <div class="col-6">
+										<h5 class="card-title mt-2 ml-4 font-weight-bold" style="font-size: 14px;">
+											@if(request('searchString') && request('searchString') != '') 
+											Search result(s) for "{{ request('searchString') }}"
+										@else
+											Item List
+										@endif</h5>
+									</div>
+									<div class="col-6">
 										<div class="col-5 col-md-3 text-right p-1">
 											<span class="font-weight-bold m-1 font-responsive">TOTAL:</span>
 											<span class="badge bg-info mr-2 font-responsive" style="font-size: 13pt;">{{ number_format($items->total()) }}</span>
 										</div>
-									</div>
+									</div> --}}
+									{{-- <div class="col-md-6">
+										<div class="row">
+											<div class="col-md-6 p-1">
+												<div class="form-group m-0 w-55" id="warehouse-filter-parent" style="font-size: 11pt;">
+													<select name="warehouse" id="warehouse-filter" class="form-control">
+														
+													</select>
+												</div>
+											</div>
+											<div class="col-7 col-md-3 p-1 text-center">
+												<div class="form-group m-0r">
+													<label>
+														<input type="checkbox" class="minimal" id="cb-2" {{ (request('check_qty')) ? 'checked' : null }} >
+														
+														<span style="font-size: 12px;">Remove zero-qty items</span>
+													</label>
+												</div>
+											</div>
+											<div class="col-5 col-md-3 text-right p-1">
+												<span class="font-weight-bold m-1 font-responsive">TOTAL:</span>
+												<span class="badge bg-info mr-2 font-responsive" style="font-size: 13pt;">{{ number_format($items->total()) }}</span>
+											</div>
+										</div>
+									</div> --}}
 								</div>
-							</div>
-						</div>
-						
-						<div class="container-fluid"><!-- new table -->
-							<div class="row">
-							@forelse ($item_list as $row)
-								<div class="col-md-6 border border-color-secondary">
-									<div class="row">
-										<div class="col-md-3 display-inline-block p-2 text-center">
-											@php
-												$img = ($row['item_image_paths']) ? "/img/" . explode('.',$row['item_image_paths'][0]->image_path)[0].'.webp' : "/icon/no_img.webp";
-											@endphp
-											<a href="{{ asset('storage/') }}{{ $img }}" data-toggle="lightbox" data-gallery="{{ $row['name'] }}" data-title="{{ $row['name'] }}">
-												<img src="{{ asset('storage/') .''. $img }}" alt="{{ Illuminate\Support\Str::slug(explode('.', $img)[0], '-') }}" class="search-img img-responsive hover">
-											</a>
-
-											<div class="modal fade" id="{{ $row['name'] }}-images-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-												<div class="modal-dialog" role="document">
-													<div class="modal-content">
-														<div class="modal-header">
-														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-															<span aria-hidden="true">&times;</span>
-														</button>
+								{{-- <div id="accordion" class="col-12 p-0">
+									<div class="card m-0">
+										<div class="row">
+											<div class="col-8">
+												<button class="btn text-left d-inline pt-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+													<p class="card-title mt-2 ml-4 font-weight-bold" style="font-size: 10pt !important">
+														Advanced Filters
+													</p>
+												</button>
+											</div>
+											<div class="col-4 text-right">
+												<p class="font-weight-bold m-1 font-responsive d-inline">TOTAL: <span class="badge badge-info font-responsive">{{ number_format($items->total()) }}</span></p>
+											</div>
+										</div>
+										
+										<div id="collapseOne" class="collapse border border-outline-secondary" aria-labelledby="headingOne" data-parent="#accordion">
+											<div class="card-body p-0">
+												<div class="col-10 mx-auto">
+													<div class="row">
+														<div class="col-4">
+															<div class="input-group-append d-none d-lg-block" id="item-group-filter-parent" style="font-size: 11pt;">
+																<select id="item-group-filter" class="btn btn-default"></select>
+															</div>
 														</div>
-														<div class="modal-body">
-															<div id="image-container" class="container-fluid">
-																<div id="carouselExampleControls" class="carousel slide" data-interval="false">
-																	<div class="carousel-inner">
-																		<div class="carousel-item active">
-																			<img class="d-block w-100" id="{{ $row['name'] }}-image" src="{{ asset('storage/').$img }}" alt="{{ Illuminate\Support\Str::slug(explode('.', $img)[0], '-') }}">
-																		</div>
-																		<span class='d-none' id="{{ $row['name'] }}-image-data">0</span>
-																	</div>
-																	<a class="carousel-control-prev" href="#carouselExampleControls" onclick="prevImg('{{ $row['name'] }}')" role="button" data-slide="prev" style="color: #000 !important">
-																		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-																		<span class="sr-only">Previous</span>
-																	</a>
-																	<a class="carousel-control-next" href="#carouselExampleControls" onclick="nextImg('{{ $row['name'] }}')" role="button" data-slide="next" style="color: #000 !important">
-																		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-																		<span class="sr-only">Next</span>
-																	</a>
-																</div>
+														<div class="col-4">
+															<div class="form-group m-0 w-100" id="warehouse-filter-parent" style="font-size: 11pt;">
+																<select name="warehouse" id="warehouse-filter" class="form-control">
+																	
+																</select>
+															</div>
+														</div>
+														<div class="col-4 text-center">
+															<div class="form-group m-0r">
+																<label>
+																	<input type="checkbox" class="minimal" id="cb-2" {{ (request('check_qty')) ? 'checked' : null }} >
+																	
+																	<span style="font-size: 12px;">Remove zero-qty items</span>
+																</label>
 															</div>
 														</div>
 													</div>
 												</div>
 											</div>
-	
-											<div class="text-center" style="margin: 1px;"><br/>
-												<a href="#" class="view-item-details" data-item-code="{{ $row['name'] }}" data-item-classification="{{ $row['item_classification'] }}">
-													<div class="btn btn-primary">
-														<i class="fa fa-file"></i> <span class="d-inline d-md-none" style="font-size: 10pt">View Item Details</span>
-													</div>
-												</a>
-												<a href="#" class="cLink d-none d-xl-inline" value="Print Barcode" onClick="javascript:void window.open('/print_barcode/{{ $row['name'] }}','1445905018294','width=450,height=700,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;">
-													<div class="btn btn-warning">
-														<i class="fa fa-qrcode"></i>
-													</div>
-												</a>
-											</div>	
 										</div>
-										<div class="col-md-9 display-inline-block p-2">
-											<div class="col-md-12 p-2 text-justify">
-												<span class="font-italic item-class">{{ $row['item_classification'] }} - {!! $row['item_group'] !!}</span><br/>
-												<span class="text-justify item-name"><b>{{ $row['name'] }}</b> - {!! $row['description'] !!}<br/>
-												<b>Part No(s)</b> {{ ($row['part_nos']) ? $row['part_nos'] : '-' }} </span>
+									</div>
+								</div> --}}
+							</div>
+							
+							<div class="container-fluid"><!-- new table -->
+								<div class="row">
+								@forelse ($item_list as $row)
+									<div class="col-12">
+										<div class="row border border-outline-secondary">
+											<div class="col-1 p-2">
+												@php
+													$img = ($row['item_image_paths']) ? "/img/" . explode('.',$row['item_image_paths'][0]->image_path)[0].'.webp' : "/icon/no_img.webp";
+												@endphp
+												<a href="{{ asset('storage/') }}{{ $img }}" data-toggle="lightbox" data-gallery="{{ $row['name'] }}" data-title="{{ $row['name'] }}">
+													<img src="{{ asset('storage/') .''. $img }}" alt="{{ Illuminate\Support\Str::slug(explode('.', $img)[0], '-') }}" class="search-img img-responsive hover">
+												</a>
+
+												<div class="modal fade" id="{{ $row['name'] }}-images-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+													<div class="modal-dialog" role="document">
+														<div class="modal-content">
+															<div class="modal-header">
+															<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																<span aria-hidden="true">&times;</span>
+															</button>
+															</div>
+															<div class="modal-body">
+																<div id="image-container" class="container-fluid">
+																	<div id="carouselExampleControls" class="carousel slide" data-interval="false">
+																		<div class="carousel-inner">
+																			<div class="carousel-item active">
+																				<img class="d-block w-100" id="{{ $row['name'] }}-image" src="{{ asset('storage/').$img }}" alt="{{ Illuminate\Support\Str::slug(explode('.', $img)[0], '-') }}">
+																			</div>
+																			<span class='d-none' id="{{ $row['name'] }}-image-data">0</span>
+																		</div>
+																		<a class="carousel-control-prev" href="#carouselExampleControls" onclick="prevImg('{{ $row['name'] }}')" role="button" data-slide="prev" style="color: #000 !important">
+																			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+																			<span class="sr-only">Previous</span>
+																		</a>
+																		<a class="carousel-control-next" href="#carouselExampleControls" onclick="nextImg('{{ $row['name'] }}')" role="button" data-slide="next" style="color: #000 !important">
+																			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+																			<span class="sr-only">Next</span>
+																		</a>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+
+												<div class="text-center" style="margin: 1px;"><br/>
+													<a href="#" class="view-item-details" data-item-code="{{ $row['name'] }}" data-item-classification="{{ $row['item_classification'] }}">
+														<div class="btn btn-primary">
+															<i class="fa fa-file"></i> <span class="d-inline d-md-none" style="font-size: 10pt">View Item Details</span>
+														</div>
+													</a>
+													<a href="#" class="cLink d-none d-xl-inline" value="Print Barcode" onClick="javascript:void window.open('/print_barcode/{{ $row['name'] }}','1445905018294','width=450,height=700,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;">
+														<div class="btn btn-warning">
+															<i class="fa fa-qrcode"></i>
+														</div>
+													</a>
+												</div>
 											</div>
-											<div class="d-none d-lg-block">
+											<div class="col-5">
+												<div class="col-md-12 p-2 text-justify">
+													<span class="font-italic item-class">{{ $row['item_classification'] }} - {!! $row['item_group'] !!}</span><br/>
+													<span class="text-justify item-name"><b>{{ $row['name'] }}</b> - {!! $row['description'] !!}</span>
+													@if ($row['part_nos'])
+														<span class="text-justify item-name"><b>Part No(s)</b> {{ $row['part_nos'] }} </span>
+													@endif
+												</div>
+											</div>
+											<div class="col-5">
 												<table class="table table-sm table-bordered warehouse-table">
 													<tr>
 														<th class="text-center wh-cell">Warehouse</th>
@@ -206,101 +275,26 @@
 														</div>
 													</div>
 													@endif
-												</div>
-											</div><!-- View Consignment Warehouse -->
+												</div><!-- View Consignment Warehouse -->
+											</div>
+											<div class="col-1">{{-- Price --}}
+											</div>
 										</div>
 									</div>
-									
-									<div class="col-12 d-block d-lg-none">
-										<table class="table table-sm table-bordered warehouse-table">
-											<tr>
-												<th class="text-center wh-cell">Warehouse</th>
-												<th class="text-center qtr-cell">Reserved Qty</th>
-												<th class="text-center qtr-cell">Available Qty</th>
-											</tr>
-											@forelse($row['item_inventory'] as $inv)
-												<tr>
-													<td class="text-center" >
-														{{ $inv['warehouse'] }}
-														{{-- @if($inv['warehouse'] == $row['default_warehouse'])
-															<span class="font-italic"><small>- default</small></span>
-														@endif --}}
-													</td>
-													<td class="text-center">{{ $inv['reserved_qty'] * 1 }}  {{ $inv['stock_uom'] }}</td>
-													<td class="text-center">
-														@if($inv['available_qty'] == 0)
-															<span class="badge badge-danger" style="font-size: 14px; margin: 0 auto;">{{ $inv['available_qty'] * 1 . ' ' . $inv['stock_uom'] }}</span>
-														@elseif($inv['available_qty'] <= $inv['warehouse_reorder_level'])
-															<span class="badge badge-warning" style="font-size: 14px; margin: 0 auto;">{{ $inv['available_qty'] * 1 . ' ' . $inv['stock_uom'] }}</span>
-														@else
-															<span class="badge badge-success" style="font-size: 14px; margin: 0 auto;">{{ $inv['available_qty'] * 1 . ' ' . $inv['stock_uom'] }}</span>
-														@endif
-													</td>
-												</tr>
-											@empty
-												<tr>
-													<td colspan="12" class="text-center" style="border: none;">NO WAREHOUSE ASSIGNED</td>
-												</tr>
-											@endforelse
-										</table>
-										<div class="col-md-12"><!-- View Consignment Warehouse -->
-											@if(count($row['consignment_warehouses']) > 0)
-											<div class="text-center">
-												<a href="#" class="btn btn-primary uppercase p-1" data-toggle="modal" data-target="#mob-vcw{{ $row['name'] }}" style="font-size: 11px;">View Consignment Warehouse</a>
-											</div>
+								@empty
+									<div class="col-md-12 text-center" style="padding: 25px;">
+										<h5>No result(s) found.</h5>
+									</div>
+								@endforelse
+								</div>
+							</div><!-- new table -->
 
-											<div class="modal fade" id="mob-vcw{{ $row['name'] }}" tabindex="-1" role="dialog">
-												<div class="modal-dialog" role="document">
-													<div class="modal-content">
-														<div class="modal-header">
-															<h4 class="modal-title consignment-head">{{ $row['name'] }} - Consignment Warehouse(s) </h4>
-															<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-														</div>
-														<form></form>
-														<div class="modal-body">
-															<table class="table table-hover m-0">
-																<col style="width: 70%;">
-																<col style="width: 30%;">
-																<tr>
-																	<th class="consignment-th text-center">Warehouse</th>
-																	<th class="consignment-th text-center">Available Qty</th>
-																</tr>
-																@forelse($row['consignment_warehouses'] as $con)
-																<tr>
-																	<td class="consignment-name">{{ $con['warehouse'] }}</td>
-																	<td class="text-center">
-																		<span class="badge badge-{{ ($con['available_qty'] > 0) ? 'success' : 'danger' }}" style="font-size: 15px; margin: 0 auto;">{{ $con['actual_qty'] * 1 . ' ' . $con['stock_uom'] }}</span>
-																	</td>
-																</tr>
-																@empty
-																<tr>
-																	<td class="text-center font-italic" colspan="3">NO WAREHOUSE ASSIGNED</td>
-																</tr>
-																@endforelse
-															</table>
-														</div>
-														<div class="modal-footer">
-															<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-														</div>
-													</div>
-												</div>
-											</div>
-											@endif
-										</div>
-									</div>
-								</div>
-							@empty
-								<div class="col-md-12 text-center" style="padding: 25px;">
-									<h5>No result(s) found.</h5>
-								</div>
-							@endforelse
+							<div class="mt-3 ml-3 clearfix pagination" style="display: block;">
+								{{ $items->links() }}
 							</div>
-						</div><!-- new table -->
-
-						<div class="ml-3 clearfix pagination" style="display: block;">
-							{{ $items->links() }}
 						</div>
 					</div>
+
 				</div>
 			</div>
 		</div>
