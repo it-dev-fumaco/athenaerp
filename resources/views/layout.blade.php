a589bce4a44dbdbc1bf0962be8795bb0d8bc391b
javekulong <85483372+javekulong@users.noreply.github.com>
2022-05-27 10:38:30 +0800

    AI-264
    In Item Profile - Stock Reservation List, Pagination is not working

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1791,77 +1791,6 @@
 				view_item_details(item_code);
 			});
 
-			// $(document).on('click', '#warehouse-location-btn', function(e){
-			// 	var item_code = $(this).data('item-code');
-
-			// 	$.ajax({
-			// 		type: 'GET',
-			// 		url: '/form_warehouse_location/' + item_code,
-			// 		success: function(response){
-			// 			$('#warehouse-location').html(response);
-			// 			$('#warehouseLocationModal').modal('show');
-			// 		}
-			// 	});
-			// });
-
-			// function view_item_details(item_code){
-			// 	$('#item-preloader').removeClass('d-none');
-			// 	$('#item-detail-content').empty();
-			// 	$.ajax({
-			// 		type: 'GET',
-			// 		url: '/get_item_details/' + item_code,
-			// 		success: function(response){
-			// 			$('#item-detail-content').html(response);
-			// 			$('#item-preloader').addClass('d-none');
-			// 			$('#view-item-details-modal').modal('show');
-			// 		}
-			// 	});
-
-			// 	$("#ath_dates").daterangepicker({
-			// 		placeholder: 'Select Date Range',
-			// 		ranges: {
-			// 			'Today': [moment(), moment()],
-			// 			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-			// 			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-			// 			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-			// 			'This Month': [moment().startOf('month'), moment().endOf('month')],
-			// 			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-			// 		},
-			// 		locale: {
-			// 			// format: 'YYYY-MM-DD',
-			// 			format: 'YYYY-MMM-DD',
-			// 			separator: " to "
-            // 		},
-			// 		startDate: moment().subtract(30, 'days'), endDate: moment(),
-			// 		// startDate: '2018-06-01', endDate: moment(),
-			// 	});
-			// 	$("#ath_dates").val('');
-			// 	$("#ath_dates").attr("placeholder","Select Date Range");
-
-			// 	$("#erp_dates").daterangepicker({
-			// 		ranges: {
-			// 			'Today': [moment(), moment()],
-			// 			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-			// 			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-			// 			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-			// 			'This Month': [moment().startOf('month'), moment().endOf('month')],
-			// 			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-			// 		},
-			// 		locale: {
-			// 			format: 'YYYY-MMM-DD',
-			// 			separator: " to "
-            // 		},
-			// 		startDate: moment().subtract(30, 'days'), endDate: moment(),
-			// 	});
-
-			// 	$("#erp_dates").val('');
-			// 	$("#erp_dates").attr("placeholder","Select Date Range");
-
-			// 	get_athena_transactions(item_code);
-			// 	get_stock_ledger(item_code);
-			// 	get_stock_reservation(item_code);
-			// }
-
 			$(document).on('submit', '.cancel-modal form', function(e){
 				e.preventDefault();
 
@@ -1904,16 +1833,6 @@ var ath_src = $('#ath-src-warehouse-filter').val();
 				
 			}
 
-			// function get_stock_reservation(item_code, page){
-			// 	$.ajax({
-			// 		type: 'GET',
-			// 		url: '/get_stock_reservation/' + item_code + '?page=' + page,
-			// 		success: function(response){
-			// 			$('#stock-reservation-table').html(response);
-			// 		}
-			// 	});
-			// }
-
 			$(document).on('click', '#low-level-stocks-pagination a', function(event){
 				event.preventDefault();
 				var page = $(this).attr('href').split('page=')[1];
@@ -1930,38 +1849,23 @@ var ath_src = $('#ath-src-warehouse-filter').val();
 				event.preventDefault();
 				var item_code = $(this).closest('div').data('item-code');
 				var page = $(this).attr('href').split('page=')[1];
-				get_stock_reservation(item_code, page);
+				get_stock_reservation(page);
 			});
 
 			$(document).on('click', '#stock-reservations-pagination-2 a', function(event){
 				event.preventDefault();
 				var item_code = $(this).closest('div').data('item-code');
 				var page = $(this).attr('href').split('page=')[1];
-				get_stock_reservation(item_code, page);
+				get_stock_reservation(page);
 			});
 
 			$(document).on('click', '#stock-reservations-pagination-3 a', function(event){
 				event.preventDefault();
 				var item_code = $(this).closest('div').data('item-code');
 				var page = $(this).attr('href').split('page=')[1];
-				get_stock_reservation(item_code, page);
+				get_stock_reservation(page);
 			});
-
-	// 		function get_stock_ledger(item_code, page){
-	// 			if(item_code) {
-	// var erp_user = $('#erp-warehouse-user-filter').val();
-	// 			var erp_wh = $('#erp-warehouse-filter').val();
-	// 			var erp_d = $('#erp_dates').val();
-	// 			$.ajax({
-	// 				type: 'GET',
-	// 				url: '/get_stock_ledger/' + item_code + '?page=' + page + '&wh_user=' + erp_user + '&erp_wh=' + erp_wh + '&erp_d=' + erp_d,
-	// 				success: function(response){
-	// 					$('#stock-ledger-table').html(response);
-	// 				}
-	// 			});
-	// 			}
-	// 		}
-
+			
 			$(document).on('click', '.upload-item-image', function(e){
 				e.preventDefault();
 
