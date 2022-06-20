e24d514c5c0644b76df4330d5d5b599918f5d3af
javekulong <85483372+javekulong@users.noreply.github.com>
2022-06-20 14:51:19 +0800

    Responsiveness Adjustments

--- resources/views/consignment/supervisor/tbl_pending_submission_inventory_audit.blade.php
+++ resources/views/consignment/supervisor/tbl_pending_submission_inventory_audit.blade.php
@@ -1,12 +1,14 @@
 <table class="table table-striped">
     <thead>
-        <th class="text-center text-uppercase font-responsive p-2">Period</th>
-        <th class="text-center text-uppercase font-responsive p-2">Store</th>
+        <th class="text-center text-uppercase font-responsive p-2">
+            <span class="d-none d-lg-block">Period</span>
+            <span class="d-lg-none">Details</span>
+        </th>
+        <th class="text-center text-uppercase font-responsive p-2 d-none d-lg-table-cell">Store</th>
         <th class="text-center text-uppercase font-responsive p-2">Promodiser</th>
     </thead>
     <tbody>
         @forelse ($pending as $row)
-
             <tr>
                 <td class="font-responsive">
                     @if (!$row['beginning_inventory_date'])
@@ -14,9 +16,9 @@
                     @else
                     <span class="d-block {{ $row['is_late'] ? 'text-danger' : '' }}">{{ $row['duration'] }}</span>
                     @endif
-                    
+                    <span class="d-block d-lg-none">{{ $row['store'] }}</span>
                    </td>
-                <td class="font-responsive text-center">{{ $row['store'] }}</td>
+                <td class="font-responsive text-center d-none d-lg-table-cell">{{ $row['store'] }}</td>
                 <td class="font-responsive text-center">{{ $row['promodisers'] }}</td>
             </tr>
         @empty
