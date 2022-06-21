2949aa810b0c5c439105b9fc0a3ae1113535aae3
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2022-06-21 08:15:59 +0800

    AI-299
    Dashboard for Consignment Supervisor to display reports

--- app/Http/Controllers/ConsignmentController.php
+++ app/Http/Controllers/ConsignmentController.php
@@ -1909,7 +1909,7 @@ class ConsignmentController extends Controller
                 $beginning_inventory_transaction_date = array_key_exists($store, $stores_with_beginning_inventory) ? $stores_with_beginning_inventory[$store] : null;
                 $last_inventory_audit_date = array_key_exists($store, $inventory_audit_per_warehouse) ? $inventory_audit_per_warehouse[$store] : null;
     
-                $duration = null;
+                $duration = $start = null;
                 if ($beginning_inventory_transaction_date) {
                     $start = Carbon::parse($beginning_inventory_transaction_date);
                 }
