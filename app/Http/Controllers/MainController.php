f3e89ed33f0e3321f12d99d2bf6bcca915af2e06
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2022-06-23 15:06:39 +0800

    AI-312
    In dashboard, display pending incoming items to receive for promodiser

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -231,9 +231,11 @@ class MainController extends Controller
                     ->where('ste.transfer_as', 'Consignment')
                     ->where('ste.purpose', 'Material Transfer')
                     ->whereIn('ste.item_status', ['For Checking', 'Issued'])
-                    ->whereIn('ste.to_warehouse', $assigned_consignment_store)
-                    ->whereIn('ste.item_status', ['For Checking', 'Issued'])
-                    ->where('sted.consignment_status', '!=', 'Received')
+                    ->whereIn('sted.t_warehouse', $assigned_consignment_store)
+                    ->where(function($q) {
+                        $q->whereNull('sted.consignment_status')
+                        ->orWhere('sted.consignment_status', '!=', 'Received');
+                    })
                     ->select('ste.name', 'ste.delivery_date', 'ste.item_status', 'ste.from_warehouse', 'ste.to_warehouse', 'ste.creation', 'sted.item_code', 'sted.description', 'sted.transfer_qty', 'sted.stock_uom', 'sted.basic_rate', 'sted.consignment_status')
                     ->orderBy('ste.creation', 'desc')->get();
 
