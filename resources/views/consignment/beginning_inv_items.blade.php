15d43a506e9d637d6245a3ce5036f445cb19d32b
javekulong <85483372+javekulong@users.noreply.github.com>
2022-08-12 11:24:30 +0800

    AI-449
    In Beginning Inventory, unable to submit beginning inventory entry

--- resources/views/consignment/beginning_inv_items.blade.php
+++ resources/views/consignment/beginning_inv_items.blade.php
@@ -185,7 +185,7 @@
                                                             <img class="d-block w-100" id="mobile-{{ $item['item_code'] }}-image" src="{{ asset('storage/').$img }}" alt="{{ Illuminate\Support\Str::slug(explode('.', $img)[0], '-') }}">
                                                         </picture>
                                                     </div>
-                                                    <span class='d-none5' id="mobile-{{ $item['item_code'] }}-image-data">0</span>
+                                                    <span class='d-none' id="mobile-{{ $item['item_code'] }}-image-data">0</span>
                                                 </div>
                                                 @if ($img_count > 1)
                                                 <a class="carousel-control-prev" href="#carouselExampleControls" onclick="prevImg('{{ $item['item_code'] }}')" role="button" data-slide="prev" style="color: #000 !important">
@@ -335,19 +335,19 @@
                 var val = 0;
 
                 if($(this).val() != ''){
-                    if($.isNumeric($(this).val())){
+                    if($.isNumeric($(this).val().replace(/,/g, ''))){
                         if(stock_value > 0){ // push in array if user puts value in stocks
                             item_codes.push(item_code);
                         }
                     }else{
-                        incorrect_item_codes.push(item);
+                        incorrect_item_codes.push(item_code);
                     }
                 }
             });
 
             $('.validate.price').each(function(){ // check price
                 var item_code = $(this).data('item-code');
-                var price = parseInt($(this).val());
+                var price = parseInt($(this).val().replace(/,/g, ''));
 
                 if($(this).val() != ''){
                     if($.isNumeric($(this).val())){
@@ -355,13 +355,13 @@
                             item_codes.push(item_code);
                         }
                     }else{
-                        incorrect_item_codes.push(item);
+                        incorrect_item_codes.push(item_code);
                     }
                 }
             });
 
             $.each(item_codes, function (e, item){ // validate stocks and price
-                if($.isNumeric($('#'+item+'-price').val()) && $.isNumeric($('#'+item+'-stock').val())){
+                if($.isNumeric($('#'+item+'-price').val().replace(/,/g, '')) && $.isNumeric($('#'+item+'-stock').val().replace(/,/g, ''))){
                     var stock = parseInt($('#'+item+'-stock').val());
                     var price = parseInt($('#'+item+'-price').val());
                     if(price > 0 && stock > 0){
