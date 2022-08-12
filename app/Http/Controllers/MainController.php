d4b0cea7c5e540a2b33c52d177be57bc916108f2
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2022-08-12 13:55:20 +0800

    AI-453
    Display Items in Search Result under warehouse category "Consigned"

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1146,7 +1146,7 @@ class MainController extends Controller
 
     public function get_select_filters(Request $request){
         $warehouses = DB::table('tabWarehouse')->where('is_group', 0)->where('disabled', 0)
-            ->where('category', 'Physical')
+            ->whereIn('category', ['Physical', 'Consigned'])
             ->selectRaw('name as id, name as text')->orderBy('name', 'asc')->get();
 
         $item_groups = DB::table('tabItem Group')->where('is_group', 0)->where('name','LIKE', '%'.$request->q.'%')
