430dd0c063758ef02f90f5409dd79ca32df454cd
javekulong <85483372+javekulong@users.noreply.github.com>
2023-02-04 11:03:19 +0800

    AI-560
    Option to hide specific item attributes to be printed in product brochure

--- resources/views/brochure/manage_item_attributes.blade.php
+++ resources/views/brochure/manage_item_attributes.blade.php
@@ -1,17 +1,20 @@
 <ul id="sortable" class="list-group">
     @foreach ($attributes as $attr)
-        <li class="list-group-item p-1">
+        <li class="list-group-item p-1 {{ $attr->hide_in_brochure ? 'hidden-attrib' : null }}">
             <div class="row">
                 <div class="col-1" style="display: flex; justify-content: center; align-items: center;">
                     <i class="fas fa-arrows-alt" style="font-size: inherit;"></i>
                 </div>
-                <div class="col-10 p-0">
-                    <input type="text" class="form-control p-1" name="attribute[{{ $attr->name }}]" value="{{ $attr->attr_name ? $attr->attr_name : $attr->attribute }}" style="font-size: inherit !important;">
+                <div class="col-9 p-0">
+                    <input type="text" class="form-control p-1" name="attribute[{{ $attr->name }}]" value="{{ $attr->attr_name ? $attr->attr_name : $attr->attribute }}" style="font-size: inherit !important; background-color: inherit">
                     <input type="hidden" name="current_attribute[{{ $attr->name }}]" value="{{ $attr->attribute }}">
                 </div>
-                <div class="col-1" style="display: flex; justify-content: center; align-items: center;">
-                    <i class="attrib-toggle far fa-eye{{ $attr->hide_in_brochure ? '-slash' : null }}" data-attrib-hidden="{{ $attr->hide_in_brochure ? 1 : 0 }}" data-attribute="{{ $attr->attribute }}" style="font-size: 10pt;"></i>
-                    <input type="hidden" class="hidden-attrib-val" name="hidden_attributes[]" value="{{ $attr->hide_in_brochure ? $attr->attribute : null }}">
+                <div class="col-2" style="display: flex; justify-content: center; align-items: center;">
+                    <div class="text-center">
+                        <input type="checkbox" class="hidden-attributes" data-attribute="{{ $attr->attribute }}" {{ $attr->hide_in_brochure ? 'checked' : null }}>
+                        <input type="hidden" name="hidden_attributes[]" value="{{ $attr->hide_in_brochure ? $attr->attribute : null }}"> <br>
+                        <small class="text-muted">{{ $attr->hide_in_brochure ? 'Unhide' : 'Hide' }}</small>
+                    </div>
                 </div>
             </div>
         </li>
@@ -27,21 +30,27 @@
     <input type="hidden" name="item_code" value="{{ $item_code }}">
     <button class="btn btn-primary" type="submit"><i class="fas fa-save"></i> Preview Changes</button>
 </div>
+<style>
+    .hidden-attrib{
+        background-color: #E6E6E6;
+    }
+</style>
 <script>
     $(document).ready(function(){
         $('#sortable').sortable();
 
-        $(document).on('click', '.attrib-toggle', function (){
+        $(document).on('click', '.hidden-attributes', function (){
             var val = '';
-            if($(this).data('attrib-hidden') == 1){
-                $(this).removeClass('fa-eye-slash').addClass('fa-eye');
-                $(this).data('attrib-hidden', 0);
-            }else{
+            if($(this).is(':checked')){
                 val = $(this).data('attribute');
-                $(this).removeClass('fa-eye').addClass('fa-eye-slash');
-                $(this).data('attrib-hidden', 1);
+                $(this).next().next().next('small').text('Unhide');
+                $(this).closest('li').addClass('hidden-attrib');
+            }else{
+                $(this).closest('li').removeClass('hidden-attrib');
+                $(this).next().next().next('small').text('Hide');
             }
-            $(this).next('.hidden-attrib-val').val(val);
+
+            $(this).next('input').val(val);
         });
     });
 </script>
