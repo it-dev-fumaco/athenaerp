3fa69d8fe448b56ff3a7db362ccd205b550e105f
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2023-07-27 16:17:08 +0800

    AI-645
    Button to generate Draft Stock Entry per Branch (Stock Transfer) for Consignment Supervisor

--- app/Console/Commands/UpdateStocks.php
+++ app/Console/Commands/UpdateStocks.php
@@ -40,7 +40,7 @@ class UpdateStocks extends Command
     {
         $submitted_pullouts = DB::table('tabConsignment Stock Entry as cste')
             ->join('tabStock Entry as ste', 'ste.name', 'cste.references')
-            ->where('cste.status', 'Pending')->where('cste.purpose', 'Pull Out')
+            ->where('cste.status', 'Pending')->whereIn('cste.purpose', ['Pull Out', 'Store Transfer'])
             ->where('ste.docstatus', 1)->select('cste.name', 'cste.source_warehouse')->get();
 
         $cste_names = collect($submitted_pullouts)->pluck('name');
