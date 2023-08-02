7b6ce0dfb6ea0189f71ce8119fc751f06c6b7765
javekulong <85483372+javekulong@users.noreply.github.com>
2023-08-02 08:25:27 +0800

    AI-657
    In Consignment - Promodiser, unable to submit inventory audit

--- app/Http/Controllers/ConsignmentController.php
+++ app/Http/Controllers/ConsignmentController.php
@@ -419,24 +419,26 @@ class ConsignmentController extends Controller
             }
 
             if ($status == 'Submitted') {
-                $submitted_by = Auth::user()->wh_user;
-                $date_submitted = $now->toDateTimeString();
+                try {
+                    $submitted_by = Auth::user()->wh_user;
+                    $date_submitted = $now->toDateTimeString();
 
-                $email_data = [
-                    'warehouse' => $request->branch,
-                    'month' => $request->month,
-                    'total_amount' => collect($sales_per_day)->sum(),
-                    'remarks' => $request->remarks,
-                    'year' => $request->year,
-                    'status' => $status,
-                    'submission_status' => $submission_status,
-                    'date_submitted' => $date_submitted
-                ];
+                    $email_data = [
+                        'warehouse' => $request->branch,
+                        'month' => $request->month,
+                        'total_amount' => collect($sales_per_day)->sum(),
+                        'remarks' => $request->remarks,
+                        'year' => $request->year,
+                        'status' => $status,
+                        'submission_status' => $submission_status,
+                        'date_submitted' => $date_submitted
+                    ];
 
-                Mail::send('mail_template.consignment_sales_report', $email_data, function($message){
-                    $message->to(str_replace('.local', '.com', Auth::user()->wh_user));
-                    $message->subject('AthenaERP - Sales Report');
-                });
+                    Mail::send('mail_template.consignment_sales_report', $email_data, function($message){
+                        $message->to(str_replace('.local', '.com', Auth::user()->wh_user));
+                        $message->subject('AthenaERP - Sales Report');
+                    });
+                } catch (\Throwable $th) {}
             }
 
             $existing_record = DB::table('tabConsignment Monthly Sales Report')->where('fiscal_year', $request->year)->where('month', $request->month)->where('warehouse', $request->branch)->first();
