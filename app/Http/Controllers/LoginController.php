6dd7c9bb97a652ca58abbb468f83640bcd17ae0b
javekulong <85483372+javekulong@users.noreply.github.com>
2024-09-23 10:51:04 +0800

    AI-725
    Add a function to generate API Credentials for Warehouse Users

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,11 +9,12 @@ use Validator;
 use DB;
 
 use App\LdapClasses\adLDAP;
+use App\Traits\ERPTrait;
 use App\Traits\GeneralTrait;
 
 class LoginController extends Controller
 {
-    use GeneralTrait;
+    use ERPTrait, GeneralTrait;
     public function view_login(){
         if(Auth::check()){
             return redirect('/');
@@ -28,53 +29,133 @@ class LoginController extends Controller
 
     public function login(Request $request){
         try {
-            // validate the info, create rules for the inputs
-            $rules = array(
-                'email' => 'required'
-            );
-
-            $email = strpos($request->email, '@fumaco.local') ? $request->email : $request->email.'@fumaco.local';
-
+            $rules = ['email' => 'required'];
+            $email = $request->email;
+        
+            if (!strpos($email, '@fumaco.local')) {
+                $email .= '@fumaco.local';
+            }
+        
             $validator = Validator::make($request->all(), $rules);
-
-            // if the validator fails, redirect back to the form
+        
             if ($validator->fails()) {
-                return redirect()->back()->withErrors($validator)
+                return redirect()->back()
+                    ->withErrors($validator)
                     ->withInput($request->except('password'));
-            }else{
-                $adldap = new adLDAP();
-                $authUser = $adldap->user()->authenticate(str_replace('@fumaco.local', null, $email), $request->password);
+            }
+        
+            $adldap = new adLDAP();
+            $username = str_replace(['@fumaco.local', '@fumaco.com'], '', $email);
+            $authUser = $adldap->user()->authenticate($username, $request->password);
 
-                if($authUser == true){
-                    $user = DB::table('tabWarehouse Users')->where('wh_user', $email)->first();
-                    if (!$user) {
-                        $user = DB::table('tabWarehouse Users')->where('wh_user', str_replace('@fumaco.local', '@fumaco.com', $email))->first();
-                    }
-
-                    if ($user) {
-                        // attempt to do the login
-                        if($user->enabled){
-                            if(Auth::loginUsingId($user->frappe_userid)){
-                                DB::table('tabWarehouse Users')->where('name', $user->name)->update(['last_login' => Carbon::now()->toDateTimeString()]);
-                                return redirect('/');
-                            } 
-                        }else{
-                            return redirect()->back()->withErrors('<span class="blink_text">Your account is disabled.</span>');
-                        }
-                    } else {        
-                        // validation not successful, send back to form 
-                        return redirect()->back()->withErrors('<span class="blink_text">Incorrect Username or Password</span>');
-                    }
-                }
-                
-                return redirect()->back()->withInput($request->except('password'))
+            if (!$authUser) {
+                return redirect()->back()
+                    ->withInput($request->except('password'))
                     ->withErrors('<span class="blink_text">Incorrect Username or Password</span>');
             }
+        
+            $user = DB::table('tabWarehouse Users')
+                ->where('wh_user', $email)
+                ->orWhere('wh_user', str_replace('@fumaco.local', '@fumaco.com', $email))
+                ->first();
+        
+            if (!$user) {
+                return redirect()->back()
+                    ->withErrors('<span class="blink_text">Incorrect Username or Password</span>');
+            }
+        
+            if (!$user->enabled) {
+                return redirect()->back()
+                    ->withErrors('<span class="blink_text">Your account is disabled.</span>');
+            }
+        
+            if (Auth::loginUsingId($user->frappe_userid)) {
+                if (!Auth::user()->api_key || !Auth::user()->api_secret) {
+                    $api_credentials = $this->generate_api_credentials();
+        
+                    if (!$api_credentials['success']) {
+                        Auth::logout();
+                        return redirect()->back()
+                            ->withInput($request->except('password'))
+                            ->withErrors('<span class="blink_text">An error occurred.<br>Please contact your system administrator.</span>');
+                    }
+                }
+        
+                DB::table('tabWarehouse Users')
+                    ->where('name', $user->name)
+                    ->update(['last_login' => Carbon::now()->toDateTimeString()]);
+        
+                return redirect('/');
+            }
+        
+            return redirect()->back()
+                ->withErrors('<span class="blink_text">Login failed. Please try again.</span>');
         } catch (adLDAPException $e) {
-            return redirect()->back()->withInput($request->except('password'))
+            return redirect()->back()
+                ->withInput($request->except('password'))
                 ->withErrors('<span class="blink_text">Cannot connect to LDAP.<br>Please contact your system administrator.</span>');
         }
     }
+
+    //     try {
+    //         // validate the info, create rules for the inputs
+    //         $rules = array(
+    //             'email' => 'required'
+    //         );
+
+    //         $email = strpos($request->email, '@fumaco.local') ? $request->email : $request->email.'@fumaco.local';
+
+    //         $validator = Validator::make($request->all(), $rules);
+
+    //         // if the validator fails, redirect back to the form
+    //         if ($validator->fails()) {
+    //             return redirect()->back()->withErrors($validator)
+    //                 ->withInput($request->except('password'));
+    //         }else{
+    //             $adldap = new adLDAP();
+    //             $authUser = $adldap->user()->authenticate(str_replace('@fumaco.local', null, $email), $request->password);
+
+    //             if($authUser == true){
+    //                 $user = DB::table('tabWarehouse Users')->where('wh_user', $email)->first();
+    //                 if (!$user) {
+    //                     $user = DB::table('tabWarehouse Users')->where('wh_user', str_replace('@fumaco.local', '@fumaco.com', $email))->first();
+    //                 }
+
+    //                 if ($user) {
+    //                     // attempt to do the login
+    //                     if($user->enabled){
+    //                         if(Auth::loginUsingId($user->frappe_userid)){
+    //                             if(!Auth::user()->api_key || !Auth::user()->api_secret){
+    //                                 $api_credentials = $this->generate_api_credentials();
+
+    //                                 if(!$api_credentials['success']){
+    //                                     Auth::logout();
+    //                                     return redirect()->back()->withInput($request->except('password'))
+    //                                     ->withErrors('<span class="blink_text">An error occured.<br>Please contact your system administrator.</span>');
+    //                                 }
+    //                             }
+
+    //                             DB::table('tabWarehouse Users')->where('name', $user->name)->update(['last_login' => Carbon::now()->toDateTimeString()]);
+
+    //                             return redirect('/');
+    //                         } 
+    //                     }else{
+    //                         return redirect()->back()->withErrors('<span class="blink_text">Your account is disabled.</span>');
+    //                     }
+    //                 } else {        
+    //                     // validation not successful, send back to form 
+    //                     return redirect()->back()->withErrors('<span class="blink_text">Incorrect Username or Password</span>');
+    //                 }
+    //             }
+                
+    //             return redirect()->back()->withInput($request->except('password'))
+    //                 ->withErrors('<span class="blink_text">Incorrect Username or Password</span>');
+    //         }
+    //     } catch (adLDAPException $e) {
+    //         return redirect()->back()->withInput($request->except('password'))
+    //             ->withErrors('<span class="blink_text">Cannot connect to LDAP.<br>Please contact your system administrator.</span>');
+    //     }
+    // }
 
     public function logout(){
         Auth::logout();
--- app/Traits/ERPTrait.php
+++ app/Traits/ERPTrait.php
@@ -2,15 +2,19 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 trait ERPTrait{
-    private function erpOperation($method, $doctype, $name = null, $body = []){
+    private function erpOperation($method, $doctype, $name = null, $body = [], $system_generated = false){
         try {
-            // ini_set('max_execution_time', 3600);
-            $erp_api_key = env('ERP_API_KEY');
-            $erp_api_secret_key = env('ERP_API_SECRET_KEY');
             $erp_api_base_url = env('ERP_API_BASE_URL');
+            $erp_api_key = Auth::user()->api_key;
+            $erp_api_secret_key = Auth::user()->api_secret;
+            if($system_generated){
+                $erp_api_key = env('ERP_API_KEY');
+                $erp_api_secret_key = env('ERP_API_SECRET_KEY');
+            }
 
             $url = "$erp_api_base_url/api/resource/$doctype";
             if($name){
@@ -25,6 +29,44 @@ trait ERPTrait{
         } catch (\Throwable $th) {
             // throw $th;
             return ['error' => 1, 'message' => $th->getMessage()];
+        }
+    }
+
+    public function generateRandomString($length = 15) {
+        return substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstuvwxyz', $length)), 0, $length);
+    }
+
+    public function generate_api_credentials(){
+        try {
+            $existing_key = $this->erpOperation('get', 'User', Auth::user()->wh_user, [], true);
+            $existing_key = isset($existing_key['data']['api_key']) ? $existing_key['data']['api_key'] : null;
+
+            $tokens = [
+                'api_key' => $existing_key ?? $this->generateRandomString(),
+                'api_secret' => $this->generateRandomString()
+            ];
+
+            $user = $this->erpOperation('put', 'User', Auth::user()->wh_user, $tokens, true);
+
+            if(!isset($user['data'])){
+                throw new \Exception('An error occured while generating API Credentials');
+            }
+
+            $warehouse_user = $this->erpOperation('put', 'Warehouse Users', Auth::user()->name, $tokens, true);
+
+            if(!isset($warehouse_user['data'])){
+                throw new \Exception('An error occured while generating API Credentials');
+            }
+
+            $user = Auth::user();
+            $user->api_key = $tokens['api_key'];
+            $user->api_secret = $tokens['api_secret'];
+            $user->save();
+            
+            return ['success' => 1, 'message' => 'API Credentials Created!'];
+        } catch (\Exception $th) {
+            // throw $th;
+            return ['success' => 0, 'message' => $th->getMessage()];
         }
     }
 }
