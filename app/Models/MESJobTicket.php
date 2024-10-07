3d4e36f3ecaf934a23cfbc93a1a6f95cba8e6278
javekulong <85483372+javekulong@users.noreply.github.com>
2024-10-07 13:55:25 +0800

    AI-729
    For Warehouse Personnel - Sales Return, use API request to issue items

---
+++ app/Models/MESJobTicket.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MESJobTicket extends Model
+{
+    use HasFactory;
+    protected $table = 'job_ticket';
+    protected $connection = 'mysql_mes';
+    protected $primaryKey = 'job_ticket_id';
+    public $timestamps = false;
+    protected $keyType = 'string';
+}
---
+++ app/Models/MESProductionOrder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MESProductionOrder extends Model
+{
+    use HasFactory;
+
+    protected $table = 'production_order';
+    protected $connection = 'mysql_mes';
+    protected $primaryKey = 'production_order';
+    public $timestamps = false;
+    protected $keyType = 'string';
+}
