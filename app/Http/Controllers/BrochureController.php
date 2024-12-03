8d15bc14654f5c68104cee8bc5f7789e187434e5
javekulong <85483372+javekulong@users.noreply.github.com>
2024-12-03 16:20:03 +0800

    Adjustment

--- app/Http/Controllers/BrochureController.php
+++ app/Http/Controllers/BrochureController.php
@@ -554,6 +554,8 @@ class BrochureController extends Controller
 
 			if($pdf){
 				set_time_limit(300);
+				ini_set('max_execution_time', 3600);
+				ini_set('memory_limit', '4096M');
 			}
 
 			$attributes_qry = DB::table('tabItem Variant Attribute as variant')
