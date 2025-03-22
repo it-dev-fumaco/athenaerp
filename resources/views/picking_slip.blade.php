4b10019a447fc8c800b118adf19be896f50d2429
Jave Judiel Kulong <85483372+javekulong@users.noreply.github.com>
2025-03-22 11:06:25 +0800

    AI-755
    Delivery data is not loading

--- resources/views/picking_slip.blade.php
+++ resources/views/picking_slip.blade.php
@@ -27,148 +27,120 @@
 								<div class="col-xl-2 col-lg-2 col-md-2">
 									<div class="form-group">
 										<select class="form-control" ng-model="searchText">
-											<option></option>
+											<option value="">All Warehouses</option>
 											<option ng-repeat="y in wh">@{{ y.name }}</option>
 										</select>
 									</div>
 								</div>
 								<div class="col-xl-2 col-lg-3 col-md-3">
 									<div class="text-center m-1">
-									   <span class="font-weight-bold">TOTAL RESULT:</span>
-									   <span class="badge bg-info" style="font-size: 12pt;">@{{ ps_filtered.length }}</span>
+										<span class="font-weight-bold">TOTAL RESULT:</span>
+										<span class="badge bg-info" style="font-size: 12pt;">@{{ ps_filtered.length }}</span>
 									</div>
 								</div>
 							</div>
 						</div>
+
 						<div class="alert m-3 text-center" ng-show="custom_loading_spinner_1">
 							<h5 class="m-0"><i class="fas fa-sync-alt fa-spin"></i> <span class="ml-2">Loading ...</span></h5>
 						</div>
+
 						<div class="card-body p-0">
 							<div class="table-responsive p-0">
 								<table class="table table-hover dashboard-table" style="font-size: 10pt;">
-									<col style="width: 15%;"><!-- Transaction -->
-									<col style="width: 40%;"><!-- Item Description -->
-									<col style="width: 10%;"><!-- Qty -->
-									<col style="width: 10%;"><!-- Delivery Date -->
-									<col style="width: 15%;"><!-- Ref. No. -->
-									<col style="width: 10%;"><!-- Actions -->
 									<thead>
 										<tr>
-											<th scope="col" class="text-center">Transaction</th>
-											<th scope="col" class="text-center d-lg-none">Details</th>
-											<th scope="col" class="text-center d-none d-lg-table-cell">Item Description</th>
-											<th scope="col" class="text-center d-none d-lg-table-cell">Qty</th>
-											<th scope="col" class="text-center d-none d-lg-table-cell">Delivery Date</th>
-											<th scope="col" class="text-center d-none d-lg-table-cell">Ref. No.</th>
-											<th scope="col" class="text-center d-none d-lg-table-cell">Actions</th>
+											<th class="text-center">Transaction</th>
+											<th class="text-center d-lg-none">Details</th>
+											<th class="text-center d-none d-lg-table-cell">Item Description</th>
+											<th class="text-center d-none d-lg-table-cell">Qty</th>
+											<th class="text-center d-none d-lg-table-cell">Delivery Date</th>
+											<th class="text-center d-none d-lg-table-cell">Ref. No.</th>
+											<th class="text-center d-none d-lg-table-cell">Actions</th>
 										</tr>
 									</thead>
 									<tbody>
-										<tr ng-repeat="x in ps_filtered = (ps | filter:searchText | filter: fltr)">
+										<tr ng-repeat="x in ps_filtered = (ps | filter:searchText | filter: fltr) | limitTo:limit">
 											<td class="text-center">
 												<span class="d-block font-weight-bold">@{{ x.creation }}</span>
 												<small class="d-block mt-1">@{{ x.name }}</small>
 												<div class="d-block d-lg-none">
-													<img src="dist/img/icon.png" ng-hide="x.type != 'picking_slip'" class="img-circle checkout update-ps"  data-id="@{{ x.id }}">
-													<img src="dist/img/icon.png"  ng-hide="x.type != 'stock_entry'" class="img-circle update-item checkout" data-id="@{{ x.id }}">
+													<img src="dist/img/icon.png" ng-if="x.type == 'picking_slip'" class="img-circle checkout update-ps" data-id="@{{ x.id }}">
+													<img src="dist/img/icon.png" ng-if="x.type == 'stock_entry'" class="img-circle update-item checkout" data-id="@{{ x.id }}">
 												</div>
 											</td>
 											<td class="text-justify">
-												<div class="d-block font-weight-bold">
-													{{-- <span class="view-item-details font-weight-bold" data-item-code="@{{ x.item_code }}">@{{ x.item_code }}</span> --}}
-													<a href="/get_item_details/@{{ x.item_code }}" target="_blank" style="color: inherit !important">
-														<span class="font-weight-bold">@{{ x.item_code }}</span>
-													</a>
-													<span class="badge badge-success mr-2" ng-if="x.status === 'Issued'">@{{ x.status }}</span>
-													<span class="badge badge-warning mr-2" ng-if="x.status === 'For Checking'">@{{ x.status }}</span>
-													<i class="fas fa-arrow-right ml-2 mr-2"></i> 
-													<span>@{{ x.warehouse }}</span>
-												</div>
+												<a href="/get_item_details/@{{ x.item_code }}" target="_blank" style="color: inherit !important">
+													<span class="font-weight-bold">@{{ x.item_code }}</span>
+												</a>
+												<span class="badge badge-success mr-2" ng-if="x.status === 'Issued'">@{{ x.status }}</span>
+												<span class="badge badge-warning mr-2" ng-if="x.status === 'For Checking'">@{{ x.status }}</span>
+												<i class="fas fa-arrow-right ml-2 mr-2"></i> 
+												<span>@{{ x.warehouse }}</span>
 												<span class="d-block">@{{ x.description }}</span>
-												<span class="d-none d-lg-block mt-3" ng-hide="x.part_nos == ''"><b>Part No(s):</b> @{{ x.part_nos }}</span>
-												<small class="d-none d-lg-block mt-2" ng-hide="x.owner == null"><b>Requested by:</b> @{{ x.owner }}</small>
-												<div class="table-responsive d-block d-lg-none">
-													<table class="table">
-														<tr>
-															<td class="p-1"><b>Part No(s):</b></td>
-															<td class="p-1">@{{ x.part_nos }}</td>
-														</tr>
-														<tr>
-															<td class="p-1"><b>Requested by:</b></td>
-															<td class="p-1">@{{ x.owner }}</td>
-														</tr>
-														<tr>
-															<td class="p-1"><b>Qty:</b></td>
-															<td class="p-1">@{{ x.qty | number:2 }} @{{ x.stock_uom }}</td>
-														</tr>
-														<tr>
-															<td class="p-1"><b>Delivery Date:</b></td>
-															<td class="p-1">
-																<span class="badge badge-danger" ng-if="x.delivery_status == 'late'" style="font-size: 10pt;">@{{ x.delivery_date }}</span>
-																<span ng-if="x.delivery_status == null">@{{ x.delivery_date }}</span>
-															</td>
-														</tr>
-														<tr>
-															<td class="p-1"><b>Ref. No.:</b></td>
-															<td class="p-1">@{{ x.sales_order }}</td>
-														</tr>
-														<tr>
-															<td colspan=2 class="text-center p-1">@{{ x.customer }}</td>
-														</tr>
-														<tr>
-															<td colspan=2 class="text-center p-1">@{{ x.classification }}</td>
-														</tr>
-													</table>
-												</div>
+												<span class="d-none d-lg-block mt-3" ng-if="x.part_nos"><b>Part No(s):</b> @{{ x.part_nos }}</span>
+												<small class="d-none d-lg-block mt-2" ng-if="x.owner"><b>Requested by:</b> @{{ x.owner }}</small>
 											</td>
 											<td class="text-center d-none d-lg-table-cell">
-												<span class="d-block" style="font-size: 14pt;">@{{ x.qty | number:2 }}</span>
-												<span class="d-block">@{{ x.stock_uom }}</span>
+												<span style="font-size: 14pt;">@{{ x.qty | number:2 }}</span><br>@{{ x.stock_uom }}
 											</td>
 											<td class="text-center d-none d-lg-table-cell">
 												<span class="badge badge-danger" ng-if="x.delivery_status == 'late'" style="font-size: 10pt;">@{{ x.delivery_date }}</span>
-												<span ng-if="x.delivery_status == null">@{{ x.delivery_date }}</span>
+												<span ng-if="!x.delivery_status">@{{ x.delivery_date }}</span>
 											</td>
 											<td class="text-center d-none d-lg-table-cell">
-												<span class="d-block">@{{ x.sales_order }}</span>
-												<span class="d-none">@{{ x.delivery_note }}</span>
+												<span>@{{ x.sales_order }}</span>
 												<small class="d-block mt-2">@{{ x.customer }}</small>
 												<small class="d-block mt-3">@{{ x.classification }}</small>
 											</td>
 											<td class="text-center d-none d-lg-table-cell">
-												<img src="dist/img/icon.png" ng-hide="!['picking_slip', 'packed_item'].includes(x.type)" class="img-circle checkout update-ps"  data-id="@{{ x.id }}" data-type="@{{ x.type }}">
-												<img src="dist/img/icon.png"  ng-hide="x.type != 'stock_entry'" class="img-circle update-item checkout" data-id="@{{ x.id }}">
+												<img src="dist/img/icon.png" ng-if="['picking_slip', 'packed_item'].includes(x.type)" class="img-circle checkout update-ps" data-id="@{{ x.id }}" data-type="@{{ x.type }}">
+												<img src="dist/img/icon.png" ng-if="x.type == 'stock_entry'" class="img-circle update-item checkout" data-id="@{{ x.id }}">
 											</td>
 										</tr>
 									</tbody>
 								</table>
+
+								<!-- Load More Button -->
+								<div class="text-center p-3">
+                                    <button id="load-more-btn" class="btn btn-primary" ng-click="loadMore()">Load More</button>
+                                    <div id="load-more-spinner" class="spinner-border text-primary d-none" role="status">
+                                        <span class="sr-only">Loading...</span>
+                                    </div>
+                                </div>
+
 							</div>
 						</div>
-					</div>
+
+					</div> <!-- card -->
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<!-- Modals (No changes) -->
 <div class="modal fade" id="ste-modal">
 	<form method="POST" action="/submit_transaction">
 		@csrf
 		<div class="modal-dialog" style="min-width: 35% !important;"></div>
 	</form>
 </div>
+
 <div class="modal fade" id="ps-modal">
 	<form method="POST" action="/checkout_picking_slip">
 		@csrf
 		<div class="modal-dialog" style="min-width: 35% !important;"></div>
 	</form>
 </div>
+
 <div class="modal fade" id="cancel-ste-modal">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Cancel Issued Item</h5>
+            <h5 class="modal-title">Cancel Issued Item</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+                <span>&times;</span>
             </button>
             </div>
             <div class="modal-body">
@@ -415,21 +387,67 @@
 		}
   	});
 
-	  var app = angular.module('myApp', []);
-	  app.controller('stockCtrl', function($scope, $http, $interval, $window, $location) {
-      $http.get("/get_parent_warehouses").then(function (response) {
-        $scope.wh = response.data.wh;
-      });
-      
-      $scope.loadData = function(){
-        $scope.custom_loading_spinner_1 = true;
-        $http.get("/view_deliveries?arr=1").then(function (response) {
-          $scope.ps = response.data.picking;
-          $scope.custom_loading_spinner_1 = false;
+    $('#load-more-btn').on('click', function() {
+        var $btn = $(this);
+        var $spinner = $('#load-more-spinner');
+
+        // Disable the button and show the spinner
+        $btn.prop('disabled', true);
+        $spinner.removeClass('d-none');
+
+        // Call the AngularJS loadMore function
+        angular.element('#anglrCtrl').scope().loadMore();
+
+        // Re-enable the button and hide the spinner after data is loaded
+        angular.element('#anglrCtrl').scope().$watch('custom_loading_spinner_1', function(newVal, oldVal) {
+            if (!newVal) {
+                $btn.prop('disabled', false);
+                $spinner.addClass('d-none');
+            }
         });
-      }
-    
-      $scope.loadData();
-	 });
+    });
+
+    var app = angular.module('myApp', []);
+    app.controller('stockCtrl', function($scope, $http) {
+        $scope.ps = [];
+        $scope.currentPage = 1;
+        $scope.hasMore = true;
+        $scope.custom_loading_spinner_1 = false;
+
+        $http.get("/get_parent_warehouses").then(function(response) {
+            $scope.wh = response.data.wh;
+        });
+
+        $scope.loadData = function(loadMore = false) {
+            if (!loadMore) {
+                $scope.ps = [];      // Reset if not loading more
+                $scope.currentPage = 1;
+                $scope.hasMore = true;
+            }
+            $scope.custom_loading_spinner_1 = true;
+
+            $http.get("/view_deliveries?arr=1&page=" + $scope.currentPage).then(function(response) {
+                if (response.data.picking.length > 0) {
+                    $scope.ps = $scope.ps.concat(response.data.picking);
+                    $scope.currentPage++;
+                    $scope.hasMore = $scope.currentPage <= response.data.pagination.last_page;
+                } else {
+                    $scope.hasMore = false;
+                }
+                $scope.custom_loading_spinner_1 = false;
+            }).catch(function() {
+                $scope.custom_loading_spinner_1 = false;
+            });
+        };
+
+        $scope.loadMore = function() {
+            if ($scope.hasMore) {
+                $scope.loadData(true);
+            }
+        };
+
+        $scope.loadData(); // Initial load
+    });
+
 </script>
 @endsection
